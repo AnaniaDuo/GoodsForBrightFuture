6ae6e66af3a7042cfd19323f213f3925cbede837
Kaia Duong <94882982+AnaniaDuo@users.noreply.github.com>
2022-05-18 15:32:28 +0000

    add store

---
+++ _ main.sh
@@ -0,0 +1,9 @@
+# npm init
+# npm i express react react-dom
+# npm i webpack webpack-cli --save-dev
+# npm i @babel/core @babel/preset-env @babel/preset-react babel-loader --save-dev
+# npm i css-loader style-loader --save-dev
+# npm i react-router-dom
+
+npm run build
+npm run backend
